3e412d23284d3204f8c03237df580070c2c158d6
blackholeearth <blackholeearth@users.noreply.github.com>
2025-05-13 16:39:19 +0300

    wpf app test

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+    }
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+            var win1 = new Window1();
+
+            win1.Show();
+        }
+
+
+
+
+
+    }
+}
