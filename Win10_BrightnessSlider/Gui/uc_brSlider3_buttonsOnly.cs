815216485324c1edcd83ba3de5ba09d91d624dce
blackholeearth <blackholeearth@users.noreply.github.com>
2025-12-17 12:07:30 +0300

    bugfix: buttons - (for DXVA slow one,) setbrigness has to be "_isMsDown:false"
    NOT.. downside is it that + - button clicks are  slower if user clicks too fast.

--- Win10_BrightnessSlider/Gui/uc_brSlider3_buttonsOnly.cs
+++ Win10_BrightnessSlider/Gui/uc_brSlider3_buttonsOnly.cs
@@ -185,7 +185,7 @@ namespace Win10_BrightnessSlider
         public void Slider_SetBrightness(int value  )
         {
             var safevalue = (int)MathFn.Clamp(value, trackBar1.Minimum, trackBar1.Maximum); //without this. trackbar goes into negatives.
-            var val = riScreen.SetBrightness(safevalue, true);
+            var val = riScreen.SetBrightness(safevalue, _isMouseDown:false);
             if (val != -1)
             {
                 lbl_value.Text = val + "";
